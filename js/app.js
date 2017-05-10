24f57bf17b19d25d88284dd3ff86ada37ca84e2e
FiorellaQA <fiorella_90_17@hotmail.com>
2017-05-10 14:30:20 -0500

    Efecto scroll

--- js/app.js
+++ js/app.js
@@ -1,22 +1,13 @@
 var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
-   var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-   if (currentScroll < lastScrollTop){
-     document.getElementsByClassName('nav-background')[0].style.display="none";
-     document.getElementById("nav-header").classList.remove("header-in");
-     document.getElementById("nav-header").style.opacity = 0;
-   } else {
-     document.getElementsByClassName('nav-background')[0].style.display="table";
-     document.getElementById("nav-header").classList.add("header-in");
-     document.getElementById("nav-header").classList.add("solid");
-     document.getElementById("nav-header").style.opacity = 1;
-     if(currentScroll<=3){
-       document.getElementById("nav-header").classList.remove("solid");
-       document.getElementById("nav-header").classList.remove("header-in");
-     }
-   }
+	var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+	if(currentScroll==0 || lastScrollTop==0){
+		document.getElementById("nav-header").classList.remove("navSmall");
+		document.getElementById("nav-header").classList.add("content");
+    }
+   	if(currentScroll > lastScrollTop){ //Cuando bajas
+   		document.getElementById("nav-header").classList.remove("content");
+    	document.getElementById("nav-header").classList.add("navSmall");
+   	}
    lastScrollTop = currentScroll;
 }, false);
-
-
-content
