da55384d4e73888537cf6c9645f8f04bffdd0340
FiorellaQA <fiorella_90_17@hotmail.com>
2017-05-10 14:03:36 -0500

    efecto scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-/*
- * Archivo principal de JS
- */
+var lastScrollTop = 0;
+window.addEventListener("scroll", function(){
+   var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+   if (currentScroll < lastScrollTop){
+     document.getElementsByClassName('nav-background')[0].style.display="none";
+     document.getElementById("nav-header").classList.remove("header-in");
+     document.getElementById("nav-header").style.opacity = 0;
+   } else {
+     document.getElementsByClassName('nav-background')[0].style.display="table";
+     document.getElementById("nav-header").classList.add("header-in");
+     document.getElementById("nav-header").classList.add("solid");
+     document.getElementById("nav-header").style.opacity = 1;
+     if(currentScroll<=3){
+       document.getElementById("nav-header").classList.remove("solid");
+       document.getElementById("nav-header").classList.remove("header-in");
+     }
+   }
+   lastScrollTop = currentScroll;
+}, false);
+
+
+content
